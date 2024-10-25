b254f8280a891ce10fcb07b0ffd4601dad6b2477
wang890 <wisecash@126.com>
2024-10-25 20:14:18 +0800

    add hello_world.ipynb, modificaion mainly in hello_world.jl

--- why/hello_world.jl
+++ why/hello_world.jl
@@ -3,28 +3,51 @@
 # Alt + Shift +Ener 在 REPL 运行鼠标所在的节
 
 using DataFrames
-using ArrayLayouts
+# using ArrayLayouts
 using LinearAlgebra
-
-
-function f(x)
-    x*x
-end
-b = f(2)
+using General.Aux
 
 ##
-c=3
-cc=33
 
+"""
+f(x): 计算平方值
+"""
+function f(x)
+    prompt = "计算平方值"
+    x*x
+end
+
+a = f(2)
+
+##
+b=2+1im
+
+b |> typeof |> display
+
+b |> typeof |> fieldnames |> display
+
+b |> propertynames |> display
+
+c=3.0
+
+@logt a b c
+
+@logt a, b, c 
 
 ##
 
 size = Sys.WORD_SIZE
 
 str = "Hello World"
+println(str)
 
 arx = [1 3
+
                2 4]
 df = DataFrame(arx, :auto)
+
 v = view(Symmetric(arx),:,:)
-aaa = 3
+
+@doc Symmetric
+
+d = 4
